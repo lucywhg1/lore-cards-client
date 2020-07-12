b12823e8463368e9d3323cb0ed31d9457d5eef3e
Lucy Waggoner <lucywhg1@gmail.com>
2020-07-12 08:51:27 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import logo from "./logo.svg";
-import { MemoryRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
@@ -12,45 +12,37 @@ import { LinkContainer } from "react-router-bootstrap";
 
 const App: React.FC = () => {
   return (
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">
-          Welcome To React-Bootstrap TypeScript Example
-        </h1>
-        <h2>
-          Current Page is{" "}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </h2>
-        <h2>
-          Navigate to{" "}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-          </ButtonToolbar>
-        </h2>
-      </Jumbotron>
-      <h2>Buttons</h2>
-      <ButtonsShowcase />
-      <h2>Toasts</h2>
-      <ToastsShowcase />
-    </Container>
+    <BrowserRouter>
+      <Container className="p-3">
+        <Jumbotron>
+          <h1 className="header">
+            Welcome To React-Bootstrap TypeScript Example
+          </h1>
+          <h2>
+            Current Page is{" "}
+            <Switch>
+              <Route path="/about"></Route>
+              <Route path="/users"></Route>
+              <Route path="/"></Route>
+            </Switch>
+          </h2>
+          <h2>
+            Navigate to{" "}
+            <ButtonToolbar className="custom-btn-toolbar">
+              <LinkContainer to="/">
+                <Button>Home</Button>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Button>About</Button>
+              </LinkContainer>
+              <LinkContainer to="/users">
+                <Button>Users</Button>
+              </LinkContainer>
+            </ButtonToolbar>
+          </h2>
+        </Jumbotron>
+      </Container>
+    </BrowserRouter>
   );
 };
 
